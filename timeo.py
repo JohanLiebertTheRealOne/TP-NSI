41d4dba3ff4cb0fd6d9c322305a82600fcf7d47b
JohanLiebertTheRealOne <gutsbuthalal@gmail.com>
2025-04-20 18:57:39 +0200

    Asserts dans la fonction récursive taille [24-NSI-01]

--- timeo.py
+++ timeo.py
@@ -4,3 +4,19 @@ def taille(arbre:dict,lettre)->int:
     gauche = arbre[lettre][0]
     droite = arbre[lettre][1]
     return 1+taille(arbre,gauche) + taille(arbre,droite)
+
+a = {
+    'F': ['B', 'G'],
+    'B': ['A', 'D'],
+    'A': ['', ''],
+    'D': ['C', 'E'],
+    'C': ['', ''],
+    'E': ['', ''],
+    'G': ['', 'I'],
+    'I': ['', 'H'],
+    'H': ['', '']
+}
+
+assert taille(a,'F')==9
+assert taille(a,'B')==5
+assert taille(a,'I')==2
