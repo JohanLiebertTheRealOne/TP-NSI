d359d0768a00b7ce0bbea92165a2731e86540021
JohanLiebertTheRealOne <gutsbuthalal@gmail.com>
2025-05-06 12:47:16 +0200

    Stop using ChatGPT

--- timeo.py
+++ timeo.py
@@ -105,3 +105,56 @@ def recherche(tab:list,n:int):
     return None
 
 assert recherche([2,4],2)==0
+
+def distance_carre(point1, point2):
+    """ Calcule et renvoie la distance au carre entre
+    deux points."""
+    return (point1[0]-point2[0])**2 + (point1[1]-point2[1])**2
+
+def point_le_plus_proche(depart, tab):
+    """ Renvoie les coordonnées du premier point du tableau tab se
+    trouvant à la plus courte distance du point depart."""
+    min_point = tab[0]
+    min_dist = distance_carre(min_point,depart)
+    for i in range(1, len(tab)):
+        if distance_carre(tab[i], depart) < min_dist:
+            min_point = tab[i]
+            min_dist = distance_carre(min_point,depart)
+    return min_point
+
+assert distance_carre((1, 0), (5, 3))==25
+assert point_le_plus_proche((5, 2), [(7, 9), (2, 5), (5, 2)])==(5, 2)
+
+def moyenne(notes:list)->float:
+    somme = 0
+    n = len(notes)
+    for note in notes:
+        somme += note[0]*note[1]
+    return somme/n
+
+class Noeud:
+    def __init__(self, etiquette, gauche, droit):
+        self.v = etiquette
+        self.gauche = gauche
+        self.droit = droit
+
+a = Noeud(1, Noeud(4, None, None),
+Noeud(0, None,
+Noeud(7, None, None)))
+
+def taille(arb:Noeud):
+    if arb==None:
+        return 0
+    return 1 + taille(arb.gauche) + taille(arb.droit)
+
+def ajoute(indice, element, tab):
+    '''Renvoie un nouveau tableau obtenu en insérant
+    element à l'indice indice dans le tableau tab.'''
+    nbre_elts = len(tab)
+    tab_ins = [0] * (nbre_elts + 1)
+    for i in range(indice):
+        tab_ins[i] = element
+        tab_ins[...] = ...
+    for i in range(indice + 1, nbre_elts + 1):
+        tab_ins[i] = ...
+    return tab_ins
